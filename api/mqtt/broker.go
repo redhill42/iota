656fcd051dde3e43bdcb53ac439c73961843239c
daniel <daniel.yuan@me.com>
2020-11-20 20:40:41 +0800

    发送MQTT应答消息

--- api/mqtt/broker.go
+++ api/mqtt/broker.go
@@ -18,6 +18,7 @@ type Broker struct {
 	client mqtt.Client
 	Mux    *mux.Router
 	qos    byte
+	pubQ   chan mqtt.Token
 }
 
 func NewBroker() (*Broker, error) {
@@ -30,20 +31,33 @@ func NewBroker() (*Broker, error) {
 	}
 
 	// Subscribe mqtt topic and forward to API server. The topic has the
-	// following pattern
+	// following pattern:
 	//
-	//    api / <ver> / <token> / <verb> / #
-	//            ^        ^        ^      ^-- extra components in request URI
-	//            |        |        |-- HTTP methods: GET, POST, DELETE
+	//    api / <ver> / <token> / <path> [ /request/$request_id ]
+	//            ^        ^        ^
+	//            |        |        |-- path of request uri
 	//            |        |-- device access token
 	//            |-- API version number, v1, v2, etc
 	//
-	// for example, the MQTT topic "api/v1/XXX/post/me/attributes" will
+	// for example, the MQTT topic "api/v1/XXX/me/attributes" will
 	// forwarded to API server with URL "/api/v1/me/attributes".
+	//
+	// If the path ends with "/request/$request_id", where $request_id is a request
+	// identifier allocated by client, then this is a GET request. Client must subscribe
+	// to a response topic to recieve response message. The response topic has the form
+	//    <token>/<path>/response/$request_id
+	// Note that the response topic doesn't contains "api/v1" prefix.
+	//
+	// for example, to get device attributes, device send an empty message to
+	// "api/v1/XXX/me/attributes/request/1" and subscribe to "XXX/me/attributes/response/1"
+	// to receive the result.
 
-	if token := broker.client.Subscribe("api/+/+/+/#", broker.qos, broker.serveMQTT); token.Wait() && token.Error() != nil {
+	if token := broker.client.Subscribe("api/+/+/#", broker.qos, broker.serveMQTT); token.Wait() && token.Error() != nil {
 		return nil, token.Error()
 	}
+
+	broker.pubQ = make(chan mqtt.Token, 100)
+	go broker.drainPubQ()
 
 	return broker, nil
 }
@@ -84,11 +98,14 @@ func (broker *Broker) configure() *mqtt.ClientOptions {
 }
 
 func (broker *Broker) Close() {
+	broker.pubQ <- nil
 	broker.client.Disconnect(250)
 }
 
 type fakeWriter struct {
-	header http.Header
+	header     http.Header
+	body       bytes.Buffer
+	statucCode int
 }
 
 func (w *fakeWriter) Header() http.Header {
@@ -99,27 +116,39 @@ func (w *fakeWriter) Header() http.Header {
 }
 
 func (w *fakeWriter) Write(p []byte) (int, error) {
-	// discard output
-	return len(p), nil
+	return w.body.Write(p)
 }
 
 func (w *fakeWriter) WriteHeader(statusCode int) {
-	// don't actually write
+	w.statucCode = statusCode
 }
 
 func (broker *Broker) serveMQTT(client mqtt.Client, msg mqtt.Message) {
-	sp := strings.SplitN(msg.Topic(), "/", 5)
-	if len(sp) != 5 {
+	sp := strings.Split(msg.Topic(), "/")
+	if len(sp) < 4 {
 		logrus.Errorf("Invalid topic: %s", msg.Topic())
 		return
 	}
-	version, token, method, path := sp[1], sp[2], strings.ToUpper(sp[3]), sp[4]
 
+	var version, token, method, path, requestId string
+
+	// Parse request topic
+	version, token = sp[1], sp[2]
+	if len(sp) >= 6 && sp[len(sp)-2] == "request" {
+		method = "GET"
+		requestId = sp[len(sp)-1]
+		path = strings.Join(sp[3:len(sp)-2], "/")
+	} else {
+		method = "POST"
+		path = strings.Join(sp[3:], "/")
+	}
+
+	// Create fake HTTP request
 	uri := "/api/" + version + "/" + path
 	body := bytes.NewReader(msg.Payload())
 	r, err := http.NewRequest(method, uri, body)
 	if err != nil {
-		logrus.WithError(err).Errorf("Failed to create request")
+		logrus.WithError(err).Error("Failed to create request")
 		return
 	}
 
@@ -128,6 +157,30 @@ func (broker *Broker) serveMQTT(client mqtt.Client, msg mqtt.Message) {
 		r.Header.Set("Content-Type", "application/json")
 	}
 
+	// Route to API server
 	w := fakeWriter{}
 	broker.Mux.ServeHTTP(&w, r)
+
+	// Send response message
+	if method == "GET" {
+		responseTopic := token + "/" + path + "/response/" + requestId
+		broker.Publish(responseTopic, w.body.Bytes())
+	}
+}
+
+func (broker *Broker) Publish(topic string, payload interface{}) {
+	broker.pubQ <- broker.client.Publish(topic, broker.qos, false, payload)
+}
+
+func (broker *Broker) drainPubQ() {
+	for {
+		t, more := <-broker.pubQ
+		if !more || t == nil {
+			break
+		}
+		_ = t.Wait()
+		if t.Error() != nil {
+			logrus.WithError(t.Error()).Error("Failed to publish message")
+		}
+	}
 }
